cba8671c69b1a103608996e4f93159e20375844f
Ameer Hamza <ameernormie@gmail.com>
2021-02-15 17:31:36 +0500

    update readme

--- internal/testutil/main.go
+++ internal/testutil/main.go
@@ -7,11 +7,11 @@ import (
 // InitDb : InitDb
 func InitDb() {
 	os.Setenv("DB_HOST", "localhost")
-	os.Setenv("DB_PORT", "1521")
-	os.Setenv("DB_SID", "XEPDB1")
-	os.Setenv("DB_USER", "KEDDBP")
-	os.Setenv("DB_PASSWORD", "yourpassword")
-	os.Setenv("DB_LIB_PATH", "/opt/oracle")
+	os.Setenv("DB_PORT", "5434")
+	os.Setenv("DB_PASSWORD", "testpassword")
+	os.Setenv("DB_DATABASE", "local")
+	os.Setenv("DB_USERNAME", "testuser")
+	os.Setenv("SECRET_KEY", "secretkey")
 	os.Setenv("ENC_KEY", "TestSecretKeyKey")
 	os.Setenv("ENC_IV", "TestSecretKeyIv2")
 }
